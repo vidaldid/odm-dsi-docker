e31fe8c49f7017d18d3efeebf4c01535f975ab2c
Jean-Philippe Orsini <orsinije@fr.ibm.com>
2017-11-14 17:18:36 +0100

    bind the port for jprofiler

--- dsi-runtime/samples/run_dsi_runtime.sh
+++ dsi-runtime/samples/run_dsi_runtime.sh
@@ -6,4 +6,4 @@
 # docker volume create --name dsi-runtime
 #
 
-docker run -p9443:9443 -vdsi-runtime:/opt/dsi/runtime/wlp --name dsi-runtime dsi-runtime /root/start.sh $*
+docker run -p9443:9443 -p9080:9080 -p8849:8849 -vdsi-runtime:/opt/dsi/runtime/wlp --name dsi-runtime dsi-runtime /root/start.sh $*
